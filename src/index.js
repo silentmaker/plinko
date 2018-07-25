70086a6993ff0e14f1574adaae407f2c108a1210
马兴骢 <maxingcong@maxingcongdeMacBook-Pro.local>
2018-07-25 20:36:37 +0800

    update service  worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import register from './serviceWorker';
+import register from './service-worker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
